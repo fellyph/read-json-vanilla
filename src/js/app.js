6d47e354638014646ab1245445d181316f17c61e
fellyph <fellyph.cintra@gmail.com>
2017-10-22 22:42:40 +0100

    fix event click and sort by name function

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,6 @@ class App {
     this.carsTemplate = ''
     this.filterCars = filterCars
     this.sortFilter = 'price'
-    this.descending = true
   }
 
   init () {
@@ -20,32 +19,35 @@ class App {
         return [...prev, ...curr.VehAvails]
       })
       this.cars.map((data) => {
-        console.log(data)
         let car = new Car(data.Vehicle.VehMakeModel['@Name'], data.Vehicle.PictureURL, data['@Status'], data.Vehicle['@FuelType'], data.TotalCharge['@RateTotalAmount'], data.TotalCharge['@CurrencyCode'])
         this.carsData.push(car)
       })
       this.sortBy(this.sortFilter)
-      this.filterCars.addEventListener('change', this.setSort)
+      this.filterCars.addEventListener('change', (e) => this.sortBy(e.target.value))
     })
     .catch((error) => {
       return {error: error}
     })
   }
 
-  setSort (e) {
-    const type = e.target.value
-    this.descending = this.sortFilter === type && !this.descending
-    this.sortBy(type) /* fix the sort */
-    this.sortFilter = type
-  }
-
   sortBy (prop) {
+    console.log(prop)
     this.carsTemplate = ''
     let orderByPrice = this.carsData
-    orderByPrice.sort((a, b) => (this.descending ? b[prop] - a[prop] : a[prop] - b[prop]))
-      .map((car) => {
-        this.carsTemplate += CarTemplate(car)
+    if (prop === 'name') {
+      orderByPrice.sort((a, b) => {
+        if (a[prop].toUpperCase() < b[prop].toUpperCase()) return -1
+        if (a[prop].toUpperCase() > b[prop].toUpperCase()) return 1
+        return 0
       })
+    } else {
+      orderByPrice.sort((a, b) => (a[prop] - b[prop]))
+    }
+
+    orderByPrice.map((car) => {
+      console.log(car)
+      this.carsTemplate += CarTemplate(car)
+    })
     this.target.innerHTML = this.carsTemplate
   }
 }
